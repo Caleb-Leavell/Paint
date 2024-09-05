96765d36ecf2e33e9ddf2da66499a1641cbc00dd
Caleb Leavell <pimaster06@gmail.com>
2024-09-05 13:12:33 -0700

    added undo/redu, reworked brush/stroke storage

--- paint.py
+++ paint.py
@@ -1,14 +1,21 @@
 import pygame
 import pygame.gfxdraw
-import math
+import copy
+from collections import deque
 
 pygame.init()
 
 screen = pygame.display.set_mode([1280, 720])
 
-points = [[]]
-brush_sizes = [3]
-brush_colors = [(0,0,0)]
+stroke = {
+    'points' : [],
+    'brush_size' : 3,
+    'brush_color' : (0,0,0)
+}
+
+strokes = [copy.deepcopy(stroke)]
+
+undo_stack = deque()
 
 class Shape:
     def display(self, pos, scale, color):
@@ -28,6 +35,19 @@ class Circle(Shape):
         pygame.gfxdraw.filled_circle(screen, int(pos.x + scale), int(pos.y + scale), scale, color)
         pygame.gfxdraw.aacircle(screen, int(pos.x + scale), int(pos.y + scale), scale, color)
 
+class Arrow_Forward(Shape):
+    def display(self, pos, scale, color):
+        pygame
+        pygame.draw.line(screen, color, pygame.Vector2(pos.x, pos.y + scale / 2), pygame.Vector2(pos.x + scale, pos.y + scale / 2), int(scale / 10))
+        pygame.draw.line(screen, color, pygame.Vector2(pos.x + scale / 1.5, pos.y + scale / 5), pygame.Vector2(pos.x + scale, pos.y + scale / 2), int(scale / 10))
+        pygame.draw.line(screen, color, pygame.Vector2(pos.x + scale / 1.5, pos.y + (scale - scale / 5)), pygame.Vector2(pos.x + scale, pos.y + scale / 2), int(scale / 10))
+
+class Arrow_Backward(Shape):
+    def display(self, pos, scale, color):
+        pygame
+        pygame.draw.line(screen, color, pygame.Vector2(pos.x, pos.y + scale / 2), pygame.Vector2(pos.x + scale, pos.y + scale / 2), int(scale / 10))
+        pygame.draw.line(screen, color, pygame.Vector2(pos.x, pos.y + scale / 2), pygame.Vector2(pos.x + scale - (scale / 1.5), pos.y + scale / 5 ), int(scale / 10))
+        pygame.draw.line(screen, color, pygame.Vector2(pos.x, pos.y + scale / 2), pygame.Vector2(pos.x + scale - (scale / 1.5), pos.y + (scale - scale / 5)), int(scale / 10))
 
 class Button:
 
@@ -65,6 +85,8 @@ red_button = Button(pygame.Vector2(115, 10), 15, Circle(), (255,0,0), pygame.Vec
 green_button = Button(pygame.Vector2(150, 10), 15, Circle(), (0,255,0), pygame.Vector2(30, 30))
 blue_button = Button(pygame.Vector2(185, 10), 15, Circle(), (0,0,255), pygame.Vector2(30, 30))
 white_button = Button(pygame.Vector2(220, 10), 15, Circle(), (255,255,255), pygame.Vector2(30, 30))
+undo_button = Button(pygame.Vector2(pygame.display.get_surface().get_width() - 85, 10), 20, Arrow_Backward(), (0,0,0), pygame.Vector2(30,30))
+redo_button = Button(pygame.Vector2(pygame.display.get_surface().get_width() - 50, 10), 20, Arrow_Forward(), (0,0,0), pygame.Vector2(30,30))
 
 
 
@@ -77,19 +99,28 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if plus_button.mouse_over():
-                brush_sizes[-1] += 1
+                stroke['brush_size'] += 1
             if minus_button.mouse_over():
-                brush_sizes[-1] -= 1
+                stroke['brush_size'] -= 1
             if black_button.mouse_over():
-                brush_colors[-1] = (0,0,0)
+                stroke['brush_color'] = (0,0,0)
             if red_button.mouse_over():
-                brush_colors[-1] = (255,0,0)
+                stroke['brush_color'] = (255,0,0)
             if green_button.mouse_over():
-                brush_colors[-1] = (0,255,0)
+                stroke['brush_color'] = (0,255,0)
             if blue_button.mouse_over():
-                brush_colors[-1] = (0,0,255)
+                stroke['brush_color'] = (0,0,255)
             if white_button.mouse_over():
-                brush_colors[-1] = (255,255,255)
+                stroke['brush_color'] = (255,255,255)
+            if undo_button.mouse_over():
+                if len(strokes) >= 2:
+                    undo_stack.append(strokes[-2])
+                    del strokes[-2]
+            if redo_button.mouse_over():
+                if(len(undo_stack) >= 1):
+                    strokes.insert(-2, undo_stack.pop())
+                
+                
 
     # easy to use variables
     width = pygame.display.get_surface().get_width()
@@ -101,23 +132,23 @@ while running:
 
     # adding positions to brush if mouse held down
     mousePos = pygame.Vector2(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-    if pygame.mouse.get_pressed()[0] and pygame.mouse.get_pos()[1] > 50:
-        points[-1].append(mousePos)
-    elif points[-1] != []:
-        points.append([])
-        brush_sizes.append(brush_sizes[-1])
-        brush_colors.append(brush_colors[-1])
-        brush_colors.append(brush_colors[-1])
-
+    if pygame.mouse.get_pressed()[0] and mouse_pos.y > 50:
+        stroke['points'].append(mouse_pos)
+        strokes[-1] = copy.deepcopy(stroke)
+    elif len(strokes[-1]['points']) >= 1:
+        strokes.append(copy.deepcopy(stroke))
+        strokes[-1]['points'] = []
+        stroke['points'] = []
 
     # display brushstrokes
-    for pointlist, brush_size, brush_color in zip(points, brush_sizes, brush_colors):
-         for point1, point2 in zip(pointlist, pointlist[1:]):
-             dist = point1.distance_to(point2)
-             for i in range(0, int(dist)):
+    for curr_stroke in strokes:
+        for point1, point2 in zip(curr_stroke['points'], curr_stroke['points'][1:]):
+            dist = point1.distance_to(point2)
+            for i in range(0, int(dist)):
                 pos = pygame.Vector2(map_range(i, 0, dist, point1.x, point2.x), map_range(i, 0, dist, point1.y, point2.y))
-                pygame.gfxdraw.aacircle(screen, int(pos.x), int(pos.y), brush_size, brush_color)
-                pygame.gfxdraw.filled_circle(screen, int(pos.x), int(pos.y), brush_size, brush_color)
+                pygame.gfxdraw.aacircle(screen, int(pos.x), int(pos.y), curr_stroke['brush_size'], curr_stroke['brush_color'])
+                pygame.gfxdraw.filled_circle(screen, int(pos.x), int(pos.y), curr_stroke['brush_size'], curr_stroke['brush_color'])
+        
 
     #display button tray
     pygame.draw.rect(screen, (200,200,200), pygame.Rect(0, 0, width, 50))
@@ -129,9 +160,11 @@ while running:
     green_button.display()
     blue_button.display()
     white_button.display()
+    undo_button.display()
+    redo_button.display()
 
     #display mouse brush
-    pygame.draw.circle(screen, brush_colors[-1], mousePos, brush_sizes[-1])
+    pygame.draw.circle(screen, stroke['brush_color'], mousePos, stroke['brush_size'])
     
     # Flip the display
     pygame.display.flip()
